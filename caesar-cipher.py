9807e9aae6e5f2efc580f9551008bbea63e786a4
NeptuneRjo <rjo9000@outlook.com>
2021-09-20 02:29:44 -0400

    New files

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -8,15 +8,16 @@ def caesar_cipher():
     cipher_text = ""
 
     for i in user_input:
+        # if the character is a-z, convert it to ASCII and add the shift to it
         if i.isalpha():
             keep_alphabet = ord(i) + cipher_shift
-            
+            # if the ASCII digit is higher than z after the shift, bring it back down to a
             if keep_alphabet > ord('z'):
                 keep_alphabet -= 26
-            
+            # change the ASCII digit to alphabetical and place into new var
             shifted_text = chr(keep_alphabet)
             cipher_text += shifted_text
-        
+        # if the character is a digit, add it to the var
         if i.isdigit() or i.isspace():
             cipher_text += i
     
